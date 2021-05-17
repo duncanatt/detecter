f0aab9cf76cfe68f376acce35909acc4b61d2e3c
Duncan Paul Attard <duncanatt@gmail.com>
2021-05-17 17:05:41 +0200

    New hello world example to be used for 'Quickstart' section in tutorial.

---
+++ examples/erlang/src/demo/hello.erl
@@ -0,0 +1,46 @@
+%%% ----------------------------------------------------------------------------
+%%% @author Duncan Paul Attard
+%%%
+%%% @doc Hello world.
+%%%
+%%% @end
+%%% 
+%%% Copyright (c) 2021, Duncan Paul Attard <duncanatt@gmail.com>
+%%%
+%%% This program is free software: you can redistribute it and/or modify it 
+%%% under the terms of the GNU General Public License as published by the Free 
+%%% Software Foundation, either version 3 of the License, or (at your option) 
+%%% any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful, but WITHOUT 
+%%% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+%%% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+%%% more details.
+%%%
+%%% You should have received a copy of the GNU General Public License along with 
+%%% this program. If not, see <https://www.gnu.org/licenses/>.
+%%% ----------------------------------------------------------------------------
+-module(hello).
+-author("Duncan Paul Attard").
+
+%%% Includes.
+-include_lib("stdlib/include/assert.hrl").
+
+%%% Public API.
+-export([start_greet/1 ,greet/1]).
+
+
+%%% ----------------------------------------------------------------------------
+%%% Public API.
+%%% ----------------------------------------------------------------------------
+
+-spec greet(Name :: string()) -> pid().
+start_greet(Name) when is_list(Name) ->
+  spawn(?MODULE, greet, [Name]).
+
+%%% ----------------------------------------------------------------------------
+%%% Private helper functions.
+%%% ----------------------------------------------------------------------------
+
+greet(Name) ->
+  io:format("Hello there, ~s!~n", [Name]).

38ebbbaf4c2b43dc7a9b72703071078ff33c9f3d
Duncan Paul Attard <duncanatt@gmail.com>
2022-01-21 18:16:41 +0000

    Fixed a subtle bug in the label printing which was causing the derivaion to output the incorrect rule name.

--- detecter/src/synthesis/lin_analyzer.erl
+++ detecter/src/synthesis/lin_analyzer.erl
@@ -146,56 +146,56 @@
   M :: monitor(),
   PdId :: pd_id().
 derive_tau(L = {'or', _, Yes = {yes, _}, _}, PdId) ->
-  ?DEBUG(":: (~s) Reducing using axiom mDisYL: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mDisYL: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Axiom mDisYL.
   {true, {{PdId, ?M_DIS_Y_L, tau, L, Yes}, Yes}};
 
 derive_tau(L = {'or', _, _, Yes = {yes, _}}, PdId) ->
-  ?DEBUG(":: (~s) Reducing using axiom mDisYR: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mDisYR: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Axiom mDisYR.
   {true, {{PdId, ?M_DIS_Y_R, tau, L, Yes}, Yes}};
 
 derive_tau(L = {'or', _, {no, _}, M}, PdId) ->
-  ?DEBUG(":: (~s) Reducing using axiom mDisNL: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mDisNL: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Axiom mDisNL.
   {true, {{PdId, ?M_DIS_N_L, tau, L, copy_ns(L, copy_ctx(L, M))}, copy_ns(L, copy_ctx(L, M))}};
 
 derive_tau(L = {'or', _, M, {no, _}}, PdId) ->
-  ?DEBUG(":: (~s) Reducing using axiom mDisNR: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mDisNR: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Axiom mDisNR.
   {true, {{PdId, ?M_DIS_N_R, tau, L, copy_ns(L, copy_ctx(L, M))}, copy_ns(L, copy_ctx(L, M))}};
 
 derive_tau(L = {'and', _, {yes, _}, M}, PdId) ->
-  ?DEBUG(":: (~s) Reducing using axiom mConYL: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mConYL: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Axiom mConYL.
   {true, {{PdId, ?M_CON_Y_L, tau, L, copy_ns(L, copy_ctx(L, M))}, copy_ns(L, copy_ctx(L, M))}};
 
 derive_tau(L = {'and', _, M, {yes, _}}, PdId) ->
-  ?DEBUG(":: (~s) Reducing using axiom mConYR: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mConYR: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Axiom mConYR.
   {true, {{PdId, ?M_CON_Y_R, tau, L, copy_ns(L, copy_ctx(L, M))}, copy_ns(L, copy_ctx(L, M))}};
 
 derive_tau(L = {'and', _, No = {no, _}, _}, PdId) ->
-  ?DEBUG(":: (~s) Reducing using axiom mConNL: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mConNL: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Axiom mConNL.
   {true, {{PdId, ?M_CON_N_L, tau, L, No}, No}};
 
 derive_tau(L = {'and', _, _, No = {no, _}}, PdId) ->
-  ?DEBUG(":: (~s) Reducing using axiom mConNR: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mConNR: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Axiom mConNR.
-  {true, {{PdId, ?M_CON_N_L, tau, L, No}, No}};
+  {true, {{PdId, ?M_CON_N_R, tau, L, No}, No}};
 
 derive_tau(L = {rec, Env, M}, PdId) ->
 
-  ?DEBUG(":: (~s) Reducing using axiom mRec: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mRec: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % The continuation of a recursive construct is encoded in terms of a function
   % that needs to be applied to unfold the monitor. Recursive monitor
@@ -218,7 +218,7 @@ derive_tau(L = {rec, Env, M}, PdId) ->
   {true, {{PdId, ?M_REC, tau, L, copy_ctx(L, M__)}, copy_ctx(L, M__)}};
 
 derive_tau(L = {var, Env, M}, PdId) ->
-  ?DEBUG(":: (~s) Reducing using axiom mRec (var): ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Reducing using axiom mRec (var): ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Recursive variables complement recursive constructs, and are used to refer
   % to recursive monitor definitions. Identically to the recursive construct,
@@ -239,15 +239,15 @@ derive_tau(L = {var, Env, M}, PdId) ->
 
 derive_tau(L = {Op, Env, M, N}, PdId) when Op =:= 'and'; Op =:= 'or' ->
 
-  ?DEBUG(":: (~s) Trying to reduce using rule mTauL: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+  ?DEBUG(":: (~s) Trying to reduce using rule mTauL: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 %%  case derive_tau(copy_ctx(L, M), new_pdid(PdId)) of
   case derive_tau(copy_ns(L, copy_ctx(L, M)), new_pdid(PdId)) of
     false ->
-      ?DEBUG(":: (~s) Trying to reduce using rule mTauR: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+      ?DEBUG(":: (~s) Trying to reduce using rule mTauR: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 %%      case derive_tau(copy_ctx(L, N), new_pdid(PdId)) of
       case derive_tau(copy_ns(L, copy_ctx(L, N)), new_pdid(PdId)) of
         false ->
-          ?DEBUG(":: (~s) Unable to reduce futher using tau: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+          ?DEBUG(":: (~s) Unable to reduce futher using tau: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
           false;
         {true, {PdN_, N_}} ->
 
@@ -282,21 +282,21 @@ derive_tau(_, _) ->
 %%% {@returns Proof derivation and monitor continuation after one event
 %%%           reduction.
 %%% }
--spec derive_act(Event, M, PdId) -> {pd(), monitor()}
+-spec derive_event(Event, M, PdId) -> {pd(), monitor()}
   when
   Event :: event(),
   M :: monitor(),
   PdId :: pd_id().
-derive_act(Act, M = {V, _}, PdId) when V =:= yes; V =:= no ->
-  ?assertNot(Act =:= tau),
-  ?DEBUG(":: (~s) Reducing using axiom mVrd: ~s.", [str_pdid(PdId), m_to_iolist(M)]),
+derive_event(Event, M = {V, _}, PdId) when V =:= yes; V =:= no ->
+  ?assertNot(Event =:= tau),
+  ?DEBUG(":: (~s) Reducing using axiom mVrd: ~s.", [pdid_to_iolist(PdId), m_to_iolist(M)]),
 
   % Axiom mVrd.
-  {{PdId, ?M_VRD, Act, M, M}, M};
+  {{PdId, ?M_VRD, Event, M, M}, M};
 
-derive_act(Act, L = {act, Env, C, M}, PdId) ->
-  ?assertNot(Act =:= tau),
-  ?assert(C(Act)),
+derive_event(Event, L = {act, Env, C, M}, PdId) ->
+  ?assertNot(Event =:= tau),
+  ?assert(C(Event)),
   ?assert(is_function(M, 1)),
 %%  ?DEBUG(":: (~s) Reducing using rule mAct: ~s.", [str_pdid(PdId), get_str(Env)]),
 
@@ -309,14 +309,14 @@ derive_act(Act, L = {act, Env, C, M}, PdId) ->
   % monitor and its continuation.
 
   Ns = get_ns(Env),
-  L_ = set_env(L, set_ctx(Env, new_binding(get_ctx(Env), unwrap_value(Ns), Binder, Act))),
+  L_ = set_env(L, set_ctx(Env, new_binding(get_ctx(Env), unwrap_value(Ns), Binder, Event))),
 
 
-  ?DEBUG(":: (~s) Reducing using rule mAct: ~s.", [str_pdid(PdId), m_to_iolist(L_)]),
+  ?DEBUG(":: (~s) Reducing using rule mAct: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L_)]),
 
 
   % Axiom mAct.
-  M_ = M(Act),
+  M_ = M(Event),
   ?assertNot(is_function(M_)),
 
 %%  M__ = set_ns()
@@ -336,43 +336,43 @@ derive_act(Act, L = {act, Env, C, M}, PdId) ->
 %%  {{PdId, mAct, Act, L_}, copy_ctx(L_, M_)}; % Updated monitor env.
 
 %%  {{PdId, mAct, Act, L, copy_ctx(L_, M_)}, copy_ctx(L_, M_)}; % Updated monitor env.
-  {{PdId, ?M_ACT, Act, L, copy_ns(L_, copy_ctx(L_, M_))}, copy_ns(L_, copy_ctx(L_, M_))}; % Updated monitor env.
+  {{PdId, ?M_ACT, Event, L, copy_ns(L_, copy_ctx(L_, M_))}, copy_ns(L_, copy_ctx(L_, M_))}; % Updated monitor env.
 
-derive_act(Act, L = {chs, _, M, N}, PdId) ->
+derive_event(Event, L = {chs, _, M, N}, PdId) ->
   ?assert(is_tuple(M) andalso element(1, M) =:= act),
   ?assert(is_tuple(N) andalso element(1, N) =:= act),
 
-  case {is_satisfied(Act, M), is_satisfied(Act, N)} of
+  case {is_satisfied(Event, M), is_satisfied(Event, N)} of
     {true, false} ->
-      ?DEBUG(":: (~s) Reducing using rule mChsL: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+      ?DEBUG(":: (~s) Reducing using rule mChsL: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
       % Rule mChsL.
-      {PdM_, M_} = derive_act(Act, copy_ns(L, copy_ctx(L, M)), new_pdid(PdId)),
+      {PdM_, M_} = derive_event(Event, copy_ns(L, copy_ctx(L, M)), new_pdid(PdId)),
 %%      ?TRACE("---- Updated context from premises: ~p", [get_ctx(get_env(M_))]),
 %%      {{PdId, mChsL, Act, L, {pre, PdM_}}, M_};
-      {{PdId, ?M_CHS_L, Act, L, M_, {pre, PdM_}}, M_};
+      {{PdId, ?M_CHS_L, Event, L, M_, {pre, PdM_}}, M_};
 %%      {{PdId, mChsL, Act, copy_ctx(M_, L), {pre, PdM_}}, M_};
 %%      {{PdId, mChsL, Act, M_, {pre, PdM_}}, M_};
 
     {false, true} ->
-      ?DEBUG(":: (~s) Reducing using rule mChsR: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+      ?DEBUG(":: (~s) Reducing using rule mChsR: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
       % Rule mChsR.
-      {PdN_, N_} = derive_act(Act, copy_ns(L, copy_ctx(L, N)), new_pdid(PdId)),
+      {PdN_, N_} = derive_event(Event, copy_ns(L, copy_ctx(L, N)), new_pdid(PdId)),
 %%      ?TRACE("---- Updated context from premises: ~p", [get_ctx(get_env(N_))]),
 %%      {{PdId, mChsR, Act, L, {pre, PdN_}}, N_}
-      {{PdId, ?M_CHS_R, Act, L, N_, {pre, PdN_}}, N_}
+      {{PdId, ?M_CHS_R, Event, L, N_, {pre, PdN_}}, N_}
 %%      {{PdId, mChsR, Act, copy_ctx(N_, L), {pre, PdN_}}, N_}
   end;
 
-derive_act(Act, L = {Op, Env, M, N}, PdId) when Op =:= 'and'; Op =:= 'or' ->
-  ?assertNot(Act =:= tau),
-  ?DEBUG(":: (~s) Reducing using rule mPar: ~s.", [str_pdid(PdId), m_to_iolist(L)]),
+derive_event(Event, L = {Op, Env, M, N}, PdId) when Op =:= 'and'; Op =:= 'or' ->
+  ?assertNot(Event =:= tau),
+  ?DEBUG(":: (~s) Reducing using rule mPar: ~s.", [pdid_to_iolist(PdId), m_to_iolist(L)]),
 
   % Unfold respective sub-monitors. Proof derivation ID for second monitor N is
   % incremented accordingly.
-  {PdM_, M_} = derive_act(Act, copy_ns(L, copy_ctx(L, M)), new_pdid(PdId)),
-  {PdN_, N_} = derive_act(Act, copy_ns(L, copy_ctx(L, N)), inc_pdid(new_pdid(PdId))),
+  {PdM_, M_} = derive_event(Event, copy_ns(L, copy_ctx(L, M)), new_pdid(PdId)),
+  {PdN_, N_} = derive_event(Event, copy_ns(L, copy_ctx(L, N)), inc_pdid(new_pdid(PdId))),
 
 
 %%  Merged = merge_ctx(get_ctx(get_env(M_)), get_ctx(get_env(N_))),
@@ -387,7 +387,7 @@ derive_act(Act, L = {Op, Env, M, N}, PdId) when Op =:= 'and'; Op =:= 'or' ->
 
 
 %%  {{PdId, mPar, Act, L, M_, N_, {pre, PdM_}, {pre, PdN_}}, {Op, Env, M_, N_}}.
-  {{PdId, ?M_PAR, Act, L, M_, N_, {pre, PdM_}, {pre, PdN_}}, {Op, Env_, M_, N_}}.
+  {{PdId, ?M_PAR, Event, L, M_, N_, {pre, PdM_}, {pre, PdN_}}, {Op, Env_, M_, N_}}.
 %%  {{PdId, mPar, Act, L, M_, N_, {pre, PdM_}, {pre, PdN_}}, {Op, Env, M_, N_}}.
 %%  {{PdId, mPar, Act, L, M_, N_, {pre, PdM_}, {pre, PdN_}}, {Op, get_env(N_), M_, N_}}. % Use context of N_ ?
 %%  {{PdId, mPar, Act, L, {pre, PdM_}, {pre, PdN_}}, {Op, Env, M_, N_}}.
@@ -398,31 +398,37 @@ derive_act(Act, L = {Op, Env, M, N}, PdId) when Op =:= 'and'; Op =:= 'or' ->
 % it after all, since it's not necessary for the correct operation of monitors,
 % but only for the correct stringifying of monitors.
 
-reduce_tau(M, PdLst) ->
-  ?TRACE("[ Attempting a new derivation for monitor on action 'tau' ]"),
+
+
+
+reduce_tau(M, PdList) ->
+  ?TRACE("[ Attempting new derivation for monitor on internal action 'tau' ]"),
 
   case derive_tau(M, new_pdid([])) of
     false ->
 
       % No more tau reductions.
-      {PdLst, M};
+      {PdList, M};
     {true, {PdM, M_}} ->
 
       % Monitor state reduced by one tau transition. Attempt to reduce further.
-      reduce_tau(M_, [PdM | PdLst])
+      reduce_tau(M_, [PdM | PdList])
   end.
 
 % Assumes that the monitor is already in a ready state.
-analyze(Act, M, PdLst) ->
-  ?TRACE("[ Starting a new derivation for monitor on action '~w' ]", [Act]),
+analyze(Event, M, PdList) ->
+  ?TRACE("[ Starting new derivation for monitor on event '~w' ]", [Event]),
 
-  % Analyze action.
-  {PdM, M_} = derive_act(Act, M, new_pdid([])),
+  % Analyze trace event.
+  {PdM, M_} = derive_event(Event, M, new_pdid([])),
 
   % Check whether the residual monitor state can be reduced further using tau
   % transitions. This ensures that the monitor is always left in a state where
   % it is ready to analyse the next action.
-  reduce_tau(M_, [PdM | PdLst]).
+  reduce_tau(M_, [PdM | PdList]).
+
+
+
 
 
 analyze_trace(Trace, M) when is_list(Trace) ->
@@ -432,8 +438,8 @@ analyze_trace(Trace, M) when is_list(Trace) ->
 analyze_trace([], M, PdList) ->
   {PdList, M};
 
-analyze_trace([Act | Trace], M, PdList) ->
-  {PdList_, M_} = analyze(Act, M, PdList),
+analyze_trace([Event | Trace], M, PdList) ->
+  {PdList_, M_} = analyze(Event, M, PdList),
   analyze_trace(Trace, M_, PdList_).
 
 
@@ -446,17 +452,6 @@ analyze_trace([Act | Trace], M, PdList) ->
 is_satisfied(Event, {act, _, C, _M}) ->
   ?assert(is_function(_M, 1)),
   C(Event).
-
-
-% Proof derivation ID related.
-new_pdid(Id) when is_list(Id) ->
-  [1 | Id].
-
-inc_pdid([Idx | Idxs]) ->
-  [Idx + 1 | Idxs].
-
-str_pdid(Id = [_ | _]) ->
-  tl(lists:foldl(fun(Idx, Id) -> [$., integer_to_list(Idx) | Id] end, [], Id)).
 
 
 %%% ----------------------------------------------------------------------------
@@ -604,6 +599,62 @@ unwrap_value({_, Value}) ->
 %%% Monitor and proof derivation display functions.
 %%% ----------------------------------------------------------------------------
 
+% This relies on the fact that the derivation algorithm copies the context from
+% one monitor continuation to the other so inherit it. But since we are printing
+% a monitor that has not been reduce, we need to pass the context to the
+% continuation which has not been yet unfolded.
+m_to_iolist(M) ->
+
+  % Pass variable context of monitor so that monitors containing free variables
+  % are correctly stringified.
+  {ctx, Ctx} = get_ctx(get_env(M)),
+  m_to_iolist(M, [{Name, Value} || {{_, Name}, Value} <- Ctx]).
+
+m_to_iolist({yes, Env = {env, _}}, _) ->
+  unwrap_value(get_str(Env));
+m_to_iolist({no, Env = {env, _}}, _) ->
+  unwrap_value(get_str(Env));
+m_to_iolist({var, Env = {env, _}, _}, _) ->
+  unwrap_value(get_str(Env));
+m_to_iolist({act, Env = {env, _}, _, M}, Ctx) ->
+
+  % The continuation of an action is a function. In order to stringify the rest
+  % of the monitor, apply the function to unfold it. Action functions accept a
+  % single parameter.
+  M_ = M(undef),
+  [lin_7:format_ph(unwrap_value(get_str(Env)), Ctx), $., m_to_iolist(M_, Ctx)];
+m_to_iolist({chs, Env = {env, _}, M, N}, Ctx) ->
+  [$(, m_to_iolist(M, Ctx), $ , unwrap_value(get_str(Env)), $ , m_to_iolist(N, Ctx), $)];
+m_to_iolist({'or', Env = {env, _}, M, N}, Ctx) ->
+  [m_to_iolist(M, Ctx), $ , unwrap_value(get_str(Env)), $ , m_to_iolist(N, Ctx)];
+m_to_iolist({'and', Env = {env, _}, M, N}, Ctx) ->
+  [m_to_iolist(M, Ctx), $ , unwrap_value(get_str(Env)), $ , m_to_iolist(N, Ctx)];
+m_to_iolist({rec, Env = {env, _}, M}, Ctx) ->
+
+  % The continuation of a recursive construct is encoded in terms of a function
+  % that needs to be applied to unfold the monitor before stringifying it.
+  % Recursive monitor definitions do not accept parameters.
+  [unwrap_value(get_str(Env)), m_to_iolist(M(), Ctx)].
+
+
+
+
+
+%%% @private Extends the current proof derivation ID with a new sub-derivation.
+-spec new_pdid(Id :: list(integer())) -> list(integer()).
+new_pdid(Id) when is_list(Id) ->
+  [1 | Id].
+
+%%% @private Increments the ID of the current proof derivation.
+-spec inc_pdid(Id :: list(integer())) -> list(integer()).
+inc_pdid([Idx | Idxs]) ->
+  [Idx + 1 | Idxs].
+
+%%% @private Returns the proof derivation ID as an IoList where each derivation
+%%%          index is period-separated.
+-spec pdid_to_iolist(Id :: list(integer())) -> iolist().
+pdid_to_iolist(Id = [_ | _]) ->
+  tl(lists:foldl(fun(Idx, Id) -> [$., integer_to_list(Idx) | Id] end, [], Id)).
 
 
 
@@ -664,7 +715,7 @@ show_pdlist(PdList) ->
 format_pd({PdId, Rule, Act, M, M_}) ->
   Indent = length(PdId) + length(?PD_SEP),
   io_lib:format("~*s [~s, \e[1;36maxiom ~s\e[0m] ~s~n\e[0;36m~*s-(~w)->\e[0m~n~*s~s~n",
-    [Indent, ?PD_SEP, str_pdid(PdId), Rule, format_m(M), Indent + 1, "", Act, Indent + 1, "", format_m(M_)]
+    [Indent, ?PD_SEP, pdid_to_iolist(PdId), Rule, format_m(M), Indent + 1, "", Act, Indent + 1, "", format_m(M_)]
 %%    [Indent, ?PD_SEP, str_pdid(PdId), Rule, format_m2(M), Indent + 1, "", Act, Indent + 1, "", format_m2(M_)]
   );
 
@@ -675,7 +726,7 @@ format_pd({PdId, Rule, Act, M, M_, {pre, PdM}}) -> % mChs
   PdMFmt = format_pd(PdM),
   Indent = length(PdId) + length(?PD_SEP),
   [io_lib:format("~*s [~s, \e[1;36mrule ~s\e[0m] ~s~n\e[0;36m~*s-(~w)->\e[0m~n~*s~s~n",
-    [Indent, ?PD_SEP, str_pdid(PdId), Rule, format_m(M), Indent + 1, "", Act, Indent + 1, "", format_m(M_)])
+    [Indent, ?PD_SEP, pdid_to_iolist(PdId), Rule, format_m(M), Indent + 1, "", Act, Indent + 1, "", format_m(M_)])
 %%    [Indent, ?PD_SEP, str_pdid(PdId), Rule, format_m2(M), Indent + 1, "", Act, Indent + 1, "", format_m2(M_)])
     | PdMFmt
   ];
@@ -684,7 +735,7 @@ format_pd({PdId, Rule, Act, M, M_, N_, {pre, PdM}}) -> % mTauL and mTauR
   PdMFmt = format_pd(PdM),
   Indent = length(PdId) + length(?PD_SEP),
   [io_lib:format("~*s [~s, \e[1;36mrule ~s\e[0m] ~s~n\e[0;36m~*s-(~w)->\e[0m~n~*s ~s ~s ~s~n",
-    [length(PdId) + 1, "-", str_pdid(PdId), Rule, format_m(M), Indent + 1, "", Act, Indent + 1, "", format_m(M_), unwrap_value(get_str(get_env(M))), format_m(N_)])
+    [length(PdId) + 1, "-", pdid_to_iolist(PdId), Rule, format_m(M), Indent + 1, "", Act, Indent + 1, "", format_m(M_), unwrap_value(get_str(get_env(M))), format_m(N_)])
 %%    [length(PdId) + 1, "-", str_pdid(PdId), Rule, format_m2(M), Indent + 1, "", Act, Indent + 1, "", format_m2(M_), unwrap_value(get_str(get_env(M))), format_m2(N_)])
     | PdMFmt
   ];
@@ -696,7 +747,7 @@ format_pd({PdId, Rule, Act, M, M_, N_, {pre, PdM}, {pre, PdN}}) ->
   [
     [
       io_lib:format("~*s [~s, \e[1;36mrule ~s\e[0m] ~s~n\e[0;36m~*s-(~w)->\e[0m~n~*s~s ~s ~s~n",
-        [length(PdId) + 1, "-", str_pdid(PdId), Rule, format_m(M), Indent + 1, "", Act, Indent + 1, "", format_m(M_), unwrap_value(get_str(get_env(M))), format_m(N_)])
+        [length(PdId) + 1, "-", pdid_to_iolist(PdId), Rule, format_m(M), Indent + 1, "", Act, Indent + 1, "", format_m(M_), unwrap_value(get_str(get_env(M))), format_m(N_)])
 %%        [length(PdId) + 1, "-", str_pdid(PdId), Rule, format_m2(M), Indent + 1, "", Act, Indent + 1, "", format_m2(M_), unwrap_value(get_str(get_env(M))), format_m2(N_)])
       | PdMFmt
     ]
